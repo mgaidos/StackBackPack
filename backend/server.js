f3380df57f2117c6589c839e74c4e44ba3ecab2a
mgaidos <m.gaidos>
2023-05-02 09:49:27 +0200

    added Procfile for heroku

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,13 @@ mongoose
     .catch(error => console.error("Could not connect to MongoDB... ", error))
 
 app.use("/", require("./routes/router"))
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 8000
+}
+
+app.listen(port, () => {
+    console.log("Server is running on port 8000")
     
 })
